7176640cc40a7b1f170e1bd001e717b66acaca04
archsolar <m_charizard@hotmail.nl>
2024-01-02 07:12:13 +0100

    Added incomplete work

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+// Idea: check if there are user settings available else fall back to this
+TextTheme defaultTextTheme() {
+  return const TextTheme(
+    bodyMedium: TextStyle(fontSize: 25.0),
+  );
+}
+
 void main() {
   runApp(const MyApp());
 }
@@ -13,12 +20,10 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         brightness: Brightness.light,
-        /* light theme settings */
+        textTheme: defaultTextTheme(),
       ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        /* dark theme settings */
-      ),
+      darkTheme:
+          ThemeData(brightness: Brightness.dark, textTheme: defaultTextTheme()),
       themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
       // Idea: replace
@@ -60,17 +65,16 @@ class _MyHomePageState extends State<MyHomePage> {
               child: ListView(
                 children: const [
                   // Idea an animation of a plant that slowly grows with consistent usage of the app
-                  Placeholder(
-                    fallbackHeight: 200,
-                    fallbackWidth: 300,
-                  ),
-                  Row(
+                  Stack(
                     children: [
-                      Checkbox(value: false, onChanged: null),
-                      Expanded(child: Text("Add task(s) for tomorrow")),
-                      Icon(Icons.arrow_forward), // Add this line
+                      Center(child: Text("2 January 2024")),
+                      Placeholder(
+                        fallbackHeight: 200,
+                        fallbackWidth: 300,
+                      ),
                     ],
                   ),
+                  TaskEntry(),
                 ],
               ),
             ),
@@ -83,6 +87,50 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class TaskEntry extends StatefulWidget {
+  const TaskEntry({super.key});
+
+  @override
+  State<TaskEntry> createState() => _TaskEntryState();
+}
+
+class _TaskEntryState extends State<TaskEntry> {
+  bool isChecked = false;
+  String taskName = "Add task(s) for tomorrow";
+
+  @override
+  Widget build(BuildContext context) {
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.blue;
+      }
+      return Colors.red;
+    }
+
+    return Row(
+      children: [
+        Checkbox(
+          checkColor: Colors.white,
+          fillColor: MaterialStateProperty.resolveWith(getColor),
+          value: isChecked,
+          onChanged: (bool? value) {
+            setState(() {
+              isChecked = value!;
+            });
+          },
+        ),
+        Expanded(child: Text(taskName)),
+        const Icon(Icons.arrow_forward), // Add this line
+      ],
     );
   }
 }
