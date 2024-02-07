e9bce0147f2a372fb089afd8095c8d5123641d02
archsolar <m_charizard@hotmail.nl>
2024-02-07 20:27:36 +0100

    Checkpoint one

--- lib/main.dart
+++ lib/main.dart
@@ -6,20 +6,50 @@ import 'package:intl/intl.dart';
 import 'package:tomorrow_todo/components/database.dart';
 import 'package:tomorrow_todo/daily_page.dart';
 import 'package:tomorrow_todo/settings.dart';
+import 'package:tomorrow_todo/setup_widget.dart';
 import 'package:tomorrow_todo/util.dart';
 import 'providers.dart';
 
+final isTaskEmpty = StateProvider<bool>((ref) {
+  return tasks.isEmpty;
+});
+
 Future<void> main() async {
   await beforeRunApp();
-  if (tasks.isEmpty) {
-    // Open setup, setup will add a first tasks at the end.
-    runApp(ProviderScope(
-      child: AppWrap(SetupPage()),
-    ));
-  } else {
-    runApp(ProviderScope(
-      child: AppWrap(DailyPage(title: 'Tomorrow TODO')),
-    ));
+  runApp(ProviderScope(
+    child: AppWrap(StartupWidget() /* SetupParent() */),
+  ));
+}
+
+class StartupWidget extends ConsumerWidget {
+  const StartupWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final taskEmpty = ref.watch(isTaskEmpty);
+
+    return taskEmpty
+        ? AppWrap(SetupPage())
+        : AppWrap(DailyPage(title: 'Tomorrow TODO'));
+  }
+}
+
+final currentSetupWidget = StateProvider<ConsumerWidget>((ref) {
+  return Page1();
+});
+
+class Page1 extends ConsumerWidget {
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
+
+class SetupPage extends ConsumerWidget {
+  const SetupPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Placeholder();
   }
 }
 
@@ -64,164 +94,195 @@ class AppWrap extends ConsumerWidget {
 // 3. Setup remainders. "1 time at timestamp or custom"
 // 4. Would you want to enable other features? Journalling/end of day rating/
 // 5. You can now set tasks for tomorrow.
-class SetupPage extends ConsumerWidget {
-  const SetupPage({super.key});
+// final widgetProvider = StateProvider<PagesWidget>((ref) => Page1());
 
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-        body: BasicPage(
-            currentPage: Page1(), nextPage: Page2(), buttonText: "I agree"));
-  }
-}
+// class SetupParent extends ConsumerWidget {
+//   const SetupParent({super.key});
 
-class Page1 extends StatelessWidget {
-  const Page1({super.key});
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     final widget = ref.watch(widgetProvider);
+//     return Scaffold(
+//         body: BasicPage(
+//       currentPage: widget,
+//       nextPage: widget.next,
+//       buttonText: widget.buttonText,
+//     ));
+//   }
+// }
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(height: 16), // Adding some spacing
-          // Container with rules
-          Center(
-            child: Container(
-              padding: EdgeInsets.all(2),
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color.fromARGB(255, 207, 35, 136),
-                    Colors.purple,
-                    const Color.fromARGB(255, 53, 39, 176)
-                  ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Container(
-                padding: EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 20, 17, 17),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    RuleText(
-                        "1 - You need to finish all tasks before the day ends or your app will be disabled."),
-                    RuleText(
-                        "2 - There needs to be at least one continuous task by default: [add tasks for tomorrow]"),
-                    RuleText("3 - Everything is stored locally."),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+// abstract class PagesWidget extends ConsumerWidget {
+//   final Widget next;
+//   final String buttonText;
 
-class RuleText extends StatelessWidget {
-  final String text;
+//   const PagesWidget({Key? key, required this.next, required this.buttonText})
+//       : super(key: key);
+// }
 
-  const RuleText(this.text, {super.key});
+// class Page1 extends PagesWidget {
+//   Page1({super.key})
+//       : super(
+//           next:
+//               Page2(), // Replace YourWidget with the widget you want to assign
+//           buttonText: "I agree",
+//         );
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8),
-      child: Text(
-        text,
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 16,
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     return Padding(
+//       padding: const EdgeInsets.all(20.0),
+//       child: Column(
+//         mainAxisAlignment: MainAxisAlignment.start,
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//           SizedBox(height: 16), // Adding some spacing
+//           // Container with rules
+//           Center(
+//             child: Container(
+//               padding: EdgeInsets.all(2),
+//               decoration: BoxDecoration(
+//                 gradient: LinearGradient(
+//                   colors: [
+//                     Color.fromARGB(255, 207, 35, 136),
+//                     Colors.purple,
+//                     const Color.fromARGB(255, 53, 39, 176)
+//                   ],
+//                   begin: Alignment.topLeft,
+//                   end: Alignment.bottomRight,
+//                 ),
+//                 borderRadius: BorderRadius.circular(8),
+//               ),
+//               child: Container(
+//                 padding: EdgeInsets.all(16),
+//                 decoration: BoxDecoration(
+//                   color: Color.fromARGB(255, 20, 17, 17),
+//                   borderRadius: BorderRadius.circular(8),
+//                 ),
+//                 child: Column(
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   children: [
+//                     RuleText(
+//                         "1 - You need to finish all tasks before the day ends or your app will be disabled."),
+//                     RuleText(
+//                         "2 - There needs to be at least one continuous task by default: [add tasks for tomorrow]"),
+//                     RuleText("3 - Everything is stored locally."),
+//                   ],
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
 
-class Page2 extends StatelessWidget {
-  const Page2({super.key});
+// class RuleText extends StatelessWidget {
+//   final String text;
 
-  @override
-  BasicPage build(BuildContext context) {
-    return BasicPage(
-        currentPage: main(context), nextPage: Page3(), buttonText: "continue");
-  }
+//   const RuleText(this.text, {super.key});
 
-  Widget main(BuildContext context) {
-    var dateCode = 'HH:mm';
-    var date = DateFormat(dateCode).format(DateTime.now());
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.symmetric(vertical: 8),
+//       child: Text(
+//         text,
+//         style: TextStyle(
+//           color: Colors.white,
+//           fontSize: 16,
+//         ),
+//       ),
+//     );
+//   }
+// }
 
-    return Container(
-      width: double.infinity,
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: Color.fromARGB(255, 20, 17, 17),
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Center(child: Column(
-        children: [
-          RuleText("1 - Current time: $date"),
-        ],
-      )),
-    );
-  }
-}
+// class Page2 extends PagesWidget {
+//   Page2({super.key})
+//       : super(
+//           next:
+//               Page2(), // Replace YourWidget with the widget you want to assign
+//           buttonText: "I agree",
+//         );
 
-class Page3 extends StatelessWidget {
-  const Page3({super.key});
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     return BasicPage(
+//         currentPage: main(context), nextPage: Page3(), buttonText: "continue");
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
+//   Widget main(BuildContext context) {
+//     var dateCode = 'HH:mm';
+//     var date = DateFormat(dateCode).format(DateTime.now());
 
-class BasicPage extends StatelessWidget {
-  final Widget currentPage;
+//     return Container(
+//       width: double.infinity,
+//       padding: EdgeInsets.all(16),
+//       decoration: BoxDecoration(
+//         color: Color.fromARGB(255, 20, 17, 17),
+//         borderRadius: BorderRadius.circular(8),
+//       ),
+//       child: Center(
+//           child: Column(
+//         children: [
+//           RuleText("1 - Current time: $date"),
+//         ],
+//       )),
+//     );
+//   }
+// }
 
-  final Widget nextPage;
-  // final String text;
-  final String buttonText;
-  const BasicPage(
-      {super.key,
-      required this.currentPage,
-      required this.nextPage,
-      required this.buttonText});
+// class Page3 extends StatelessWidget {
+//   const Page3({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(height: 40), // Adjust the spacing as needed
-        Container(
-            height: MediaQuery.of(context).size.height * 0.7,
-            child: currentPage),
-        SizedBox(height: 16), // Adjust the spacing as needed
-        Container(
-          height: 50, // Adjust the height as needed
-          width: double.infinity,
-          child: ElevatedButton(
-            onPressed: () {
-              // Add your CONTINUE button action here
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => nextPage),
-              );
-            },
-            child: Text(buttonText),
-          ),
-        ),
-      ],
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+
+// class BasicPage extends ConsumerWidget {
+//   final Widget currentPage;
+//   final Widget? nextPage;
+//   // final String text;
+//   final String buttonText;
+//   const BasicPage(
+//       {super.key,
+//       required this.currentPage,
+//       required this.nextPage,
+//       required this.buttonText});
+
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     return Column(
+//       children: [
+//         SizedBox(height: 40), // Adjust the spacing as needed
+//         SizedBox(
+//             height: MediaQuery.of(context).size.height * 0.7,
+//             child: currentPage),
+//         SizedBox(height: 16), // Adjust the spacing as needed
+//         SizedBox(
+//           height: 50, // Adjust the height as needed
+//           width: double.infinity,
+//           child: ElevatedButton(
+//             onPressed: () {
+//               // TODO lots of work here
+//               if (nextPage == null) {
+//                 // Move on to the main page.
+//               } else {
+//                 // TODO how to correctly change this without using the notivier state thing.
+//                 final a = ref.read(widgetProvider) = Page2();
+//                 // Navigator.push(
+//                 //   context,
+//                 //   MaterialPageRoute(builder: (context) => nextPage!),
+//                 // );
+//                 ref.read();
+//               }
+//             },
+//             child: Text(buttonText),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
